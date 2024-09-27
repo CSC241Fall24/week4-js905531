15cd8151bc20708820167f65a99ff3afe35cab8f
js905531 <157529761+js905531@users.noreply.github.com>
2024-09-27 10:43:05 -0400

    Trial 1

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -4,6 +4,20 @@ public class Problem1 {
         // TODO: Implement the concatenate method
         // This method should concatenate l2 to l1 and return l1
         // Remember to create new nodes instead of directly linking l1 to l2
-        return null;
+        ListNode dummy = new ListNode(0);
+        dummy.next=l1;
+        ListNode current = dummy;
+        while (current.next != null){
+            current = current.next;
+        }
+        current.next = new ListNode(l2.val);
+        current = current.next;
+        ListNode pointer = l2;
+        while (pointer.next != null){
+            current.next = new ListNode(pointer.next.val);
+            current = current.next;
+            pointer=pointer.next;
+        }
+        return dummy.next;
     }
 }
--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -4,6 +4,19 @@ public class Problem2 {
         // TODO: Implement the insert method
         // This method should insert a new node with the given value at the specified position
         // If the position is out of bounds, insert the node at the end of the list
-        return null;
+        ListNode newNode = new ListNode(val);
+        if (head == null || position == 1) {
+            newNode.next = head;
+            return newNode;
+        }
+        ListNode current = head;
+        int currentPosition = 1;
+        while (current.next != null && currentPosition < position - 1) {
+            current = current.next;
+            currentPosition++;
+        }
+        newNode.next = current.next;
+        current.next = newNode;
+        return head;
     }
 }
